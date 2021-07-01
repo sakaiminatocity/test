dd3a84cac40d3807212ef6c2fe3d8a4684517b0c
S.Yabunaka[strCode] <yabunaka@strcode.biz>
2021-07-01 21:59:57 +0900

    2021.7.1 Test programs, upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,18 @@
 const cacheName = '5374.jp-caches';
 const urlsToCache = [
-  '/5374.jp/',
-  '/5374.jp/favicon.ico',
-  '/5374.jp/assets/css/style.css',
-  '/5374.jp/assets/css/style.css.map',
-  '/5374.jp/assets/css/customize.css',
-  '/5374.jp/assets/js/script.js',
-  '/5374.jp/assets/images/logo.jpg',
-  '/5374.jp/assets/images/00105743-TEesoV.pdf',
-  '/5374.jp/assets/images/カレンダー2018.pdf',
-  '/5374.jp/assets/images/自己搬入について.png',
-  '/5374.jp/assets/images/収集処理しないごみ.jpg',
-  '/5374.jp/assets/images/処理できないごみ等.png',
-  '/5374.jp/assets/images/直接施設搬入.jpg'
+  '/test/',
+  '/test/favicon.ico',
+  '/test/assets/css/style.css',
+  '/test/assets/css/style.css.map',
+  '/test/assets/css/customize.css',
+  '/test/assets/js/script.js',
+  '/test/assets/images/logo.jpg',
+  '/test/assets/images/00105743-TEesoV.pdf',
+  '/test/assets/images/カレンダー2018.pdf',
+  '/test/assets/images/自己搬入について.png',
+  '/test/assets/images/収集処理しないごみ.jpg',
+  '/test/assets/images/処理できないごみ等.png',
+  '/test/assets/images/直接施設搬入.jpg'
   // Test Directory.
   // '/',
   // '/favicon.ico',
